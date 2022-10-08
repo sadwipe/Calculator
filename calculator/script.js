b287452de2fccb742b3e4a4dba864aaa417478bd
sadwipe <sadwipe@gmail.com>
2022-10-09 02:26:08 +0300

    clicking numbers will display values

--- calculator/script.js
+++ calculator/script.js
@@ -1,43 +1,42 @@
 const calculator = () => {
+  const numbers = document.querySelectorAll(".number");
+  const currentNumber = document.querySelector(".current-number");
+  const previousNumber = document.querySelector(".previous-number");
+  const deleteChar = document.querySelector(".delete-last");
+  const resetResult = document.querySelector(".reset-result");
 
-const numbers = document.querySelectorAll(".number");
-const currentNumber = document.querySelector(".current-number");
-const previousNumber = document.querySelector(".previous-number");
-const deleteChar = document.querySelector(".delete-last");
-const resetResult = document.querySelector(".reset-result");
-
-// removes the last number of the currentNumber
-function updateNumber() {
-    function deleteNumber() {
-        let n = currentNumber.textContent.length;
-        currentNumber.textContent = currentNumber.textContent.slice(0, n-1);
-    };
-    deleteChar.addEventListener("click", deleteNumber);
-}
-
-
-
-// clears the results
-function resetValues() {
+  // clears the results
+  function resetValues() {
     resetResult.addEventListener("click", () => {
-        currentNumber.textContent = " ";
-        previousNumber.textContent = " ";
-    })
-}
-
-// returns the number we click on
-function returnNumber() {
-    numbers.forEach(number => {
-        number.addEventListener("click", () => {
-            return number.textContent;
-        });
+      currentNumber.textContent = " ";
+      previousNumber.textContent = " ";
     });
-};
+  }
 
-resetValues();
-returnNumber();
-updateNumber();
+  // removes the last number of the currentNumber
+  function removeNumber() {
+    function deleteNumber() {
+      let n = currentNumber.textContent.length;
+      currentNumber.textContent = currentNumber.textContent.slice(0, n - 1);
+    }
+    deleteChar.addEventListener("click", deleteNumber);
+  }
 
+  // displays the number you click on
+  function updateNumber() {
+    let n = currentNumber.textContent.length;
+    numbers.forEach((number) => [
+      number.addEventListener("click", () => {
+        if (currentNumber.textContent.length <= 9) {
+          currentNumber.textContent += number.textContent;
+        }
+      }),
+    ]);
+  }
+
+  resetValues();
+  removeNumber();
+  updateNumber();
 };
 
 calculator();

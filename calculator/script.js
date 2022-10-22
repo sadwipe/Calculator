2593a46711dc69d6c0e199cebaa4bca4df9e8f18
sadwipe <sadwipe@gmail.com>
2022-10-22 13:51:29 +0300

    C button, % button, AC button (. buttons needs fix)

--- calculator/script.js
+++ calculator/script.js
@@ -7,40 +7,73 @@ const clearButton = document.querySelector(".delete-last");
 const comma = document.querySelector("#comma");
 const equal = document.querySelector("#equal");
 
-let hasOperator = false;
+// algorythm of the calculator
+let firstValue = "";
+let hasFirstValue = false;
+let secondValue = "";
+let hasSecondValue = false;
+let previousNumber = "";
+let sign = "";
+let clickNumber = 0;
+let hasComma = false;
 
 // display
 const displayNumber = document.querySelector(".current-number");
 const displayPreviousNumber = document.querySelector(".previous-number");
 
-
-// for design part of the calculator
-const calculatorDesign = () => {
-    
-    function highlightOperators() {
-        operators.forEach(operator => {
-            operator.addEventListener("click", () => {
-                if(hasOperator == false){
-                    operator.classList.add("highlight-orange");
-                    hasOperator = true;
-                } else {
-                    operators.forEach(operator => {
-                        operator.classList.remove("highlight-orange");
-                        hasOperator = false;
-                    });
-                };
-            });
-        });
-    };
-
-    highlightOperators();
-}
-
 // for logic part of the calculator
 const calculatorLogic = () => {
 
+    // display the numbers you click (limit of 9)
+    numbers.forEach(number => {
+        number.addEventListener("click", () => {
+            if(displayNumber.textContent.length <= 9 && hasComma == true) {
+                displayNumber.textContent += number.textContent;
+            } else if(displayNumber.textContent.length <= 8 && hasComma == false) {
+                displayNumber.textContent += number.textContent;
+            };
+        });
+    });
+
+    // "AC" button
+    allClearButton.addEventListener("click", () => { 
+        displayNumber.textContent = "";
+        displayPreviousNumber.textContent = "";
+        secondValue = "";
+        firstValue = "";
+        sign = "";
+        hasFirstValue = false;
+        hasSecondValue = false;
+        clickNumber = 0;
+        hasComma = false;
+    });
+
+    // "C" button
+    clearButton.addEventListener("click", () => {
+        displayNumber.textContent = displayNumber.textContent.slice(0,-1);
+    });
+
+    // "%" button
+    percentage.addEventListener("click", () => {
+        if(displayNumber.textContent.length > 0) {
+            displayNumber.textContent = displayNumber.textContent / 100;
+        };
+        clickNumber += 1;
+        if(clickNumber == 3) {
+            displayNumber.textContent = "ERROR";
+        };
+    });
+
+    // "." button (doesn't affect number's length)
+    comma.addEventListener("click", () => {
+        if(hasComma == false) {
+            displayNumber.textContent += ".";
+            hasComma = true;
+        } else if(hasComma == true) {
+            displayNumber.textContent += "";
+        };
+    })
+
 };
 
-
-calculatorDesign();
 calculatorLogic();

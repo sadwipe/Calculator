189c034c569978e14ec81785dff4b4e1fed56977
sadwipe <sadwipe@gmail.com>
2022-10-27 23:04:03 +0300

    third try..

--- calculator/script.js
+++ calculator/script.js
@@ -1,79 +1,67 @@
 // buttons
 const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator");
-const percentage = document.querySelector("#percent");
-const allClearButton = document.querySelector(".reset-result");
-const clearButton = document.querySelector(".delete-last");
-const comma = document.querySelector("#comma");
-const equal = document.querySelector("#equal");
+const signs = document.querySelectorAll(".operator");
+const AllClearButton = document.querySelector(".reset-result");
+const ClearButton = document.querySelector(".delete-last");
+const percentButton = document.getElementById("percent");
+const commaButton = document.getElementById("comma");
+const equalButton = document.getElementById("equal");
 
-// algorythm of the calculator
+// display results
+const displayNumber = document.querySelector(".current-number");
+const previousNumber = document.querySelector(".previous-number");
+
 let firstValue = "";
-let hasFirstValue = false;
+let isFirstValue = false;
 let secondValue = "";
-let hasSecondValue = false;
-let previousNumber = "";
+let isSecondValue = false;
 let sign = "";
-let clickNumber = 0;
 let hasComma = false;
 
-// display
-const displayNumber = document.querySelector(".current-number");
-const displayPreviousNumber = document.querySelector(".previous-number");
-
-// for logic part of the calculator
-const calculatorLogic = () => {
-
-    // display the numbers you click (limit of 9)
-    numbers.forEach(number => {
-        number.addEventListener("click", () => {
-            if(displayNumber.textContent.length <= 9 && hasComma == true) {
-                displayNumber.textContent += number.textContent;
-            } else if(displayNumber.textContent.length <= 8 && hasComma == false) {
-                displayNumber.textContent += number.textContent;
-            };
-        });
-    });
-
-    // "AC" button
-    allClearButton.addEventListener("click", () => { 
-        displayNumber.textContent = "";
-        displayPreviousNumber.textContent = "";
-        secondValue = "";
-        firstValue = "";
-        sign = "";
-        hasFirstValue = false;
-        hasSecondValue = false;
-        clickNumber = 0;
-        hasComma = false;
-    });
-
-    // "C" button
-    clearButton.addEventListener("click", () => {
-        displayNumber.textContent = displayNumber.textContent.slice(0,-1);
-    });
-
-    // "%" button
-    percentage.addEventListener("click", () => {
-        if(displayNumber.textContent.length > 0) {
-            displayNumber.textContent = displayNumber.textContent / 100;
-        };
-        clickNumber += 1;
-        if(clickNumber == 3) {
-            displayNumber.textContent = "ERROR";
+// displays the numbers you click on, comma doesn't affect maximum length
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if(displayNumber.textContent.length <= 8) {
+            displayNumber.textContent += number.textContent;
+        } else if (hasComma == true && displayNumber.textContent.length <= 9) {
+            displayNumber.textContent += number.textContent;
         };
     });
+});
 
-    // "." button (doesn't affect number's length)
-    comma.addEventListener("click", () => {
-        if(hasComma == false) {
-            displayNumber.textContent += ".";
-            hasComma = true;
-        } else if(hasComma == true) {
-            displayNumber.textContent += "";
-        };
-    })
+// comma button, can be used only once and it can't be used as last char.
+commaButton.addEventListener("click", () => {
+    if(hasComma == false && displayNumber.textContent.length < 9 && displayNumber.textContent.length > 0){
+        displayNumber.textContent += ".";
+        hasComma = true;
+    };
+});
 
-};
+//AC button, clears everything and resets all variables
+AllClearButton.addEventListener("click", () => {
 
-calculatorLogic();
+    displayNumber.textContent = "";
+    previousNumber.textContent = "";
+    firstValue = "";
+    isFirstValue = false;
+    secondValue = "";
+    isSecondValue = false;
+    sign = "";
+    hasComma = false;
+    isPercent = false;
+
+})
+
+//C button, deletes last character of the number
+ClearButton.addEventListener("click", () => {
+    displayNumber.textContent = displayNumber.textContent.slice(0, -1);
+});
+
+//% button, returns the value / 100
+percentButton.addEventListener("click", () => {
+    if(displayNumber.textContent.length <= 10) {
+        displayNumber.textContent = displayNumber.textContent / 100;
+    } else {
+        displayNumber.textContent = "ERROR";
+    }
+});
